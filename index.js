4d8d884506631f9103aaee079bcd6faf8c97df8f
Eryoung-610 <eryoung610@gmail.com>
2020-06-23 21:51:20 -0700

    Second model implemented

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const db = require('./models');
 const app = express();
 
 // Routes
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('hOmIeSlIcE');
 });
 
 // Get all owners
-app.get('/owners', (req,res) => {
+app.get('/owners', (req, res) => {
     db.owner.findAll().then(owners => {
         res.send(owners);
     }).catch(err => {
@@ -17,7 +17,7 @@ app.get('/owners', (req,res) => {
 })
 
 // Solo owner by id
-app.get('/owners/:id', (req,res) => {
+app.get('/owners/:id', (req, res) => {
     db.owner.findByPk(req.params.id).then(owner => {
         res.send(owner);
     }).catch(err => {
@@ -26,7 +26,7 @@ app.get('/owners/:id', (req,res) => {
 })
 
 // Create owner
-app.post('/owners',(req,res) => {
+app.post('/owners', (req, res) => {
     db.owner.create({
         firstName: "Spongebob",
         lastName: 'Squarepants',
@@ -40,26 +40,26 @@ app.post('/owners',(req,res) => {
 })
 
 // Update owner by id
-app.put('/owners/:id', (req,res) => {
+app.put('/owners/:id', (req, res) => {
     db.owner.update({
         dlNum: 'testdlNum'
     },
-    {
-        where: {
-            id: req.params.id
-        },
-    }).then((updated) => {
-        res.send(updated)
-    }).catch(err => {
-        res.send(err);
-    })
+        {
+            where: {
+                id: req.params.id
+            },
+        }).then((updated) => {
+            res.send(updated)
+        }).catch(err => {
+            res.send(err);
+        })
 })
 
 // Destroy owner
-app.delete('/owner/:id', (req,res) =>{
+app.delete('/owner/:id', (req, res) => {
     db.owner.destroy({
         where: {
-            id:req.params.id
+            id: req.params.id
         }
     }).then((deleted) => {
         res.send("Deleted owner")
@@ -69,7 +69,7 @@ app.delete('/owner/:id', (req,res) =>{
 })
 
 // Get vehicles
-app.get('/vehicles', (req,res) => {
+app.get('/vehicles', (req, res) => {
     db.vehicle.findAll().then(vehicles => {
         res.send(vehicles)
     }).catch(err => {
@@ -78,11 +78,62 @@ app.get('/vehicles', (req,res) => {
 })
 
 // Single vehicle by id
-app.get('/vehicles/:id', (req,res) => {
+app.get('/vehicles/:id', (req, res) => {
     db.vehicle.findByPk(req.params.id).then(vehicle => {
         res.send(vehicle);
     }).catch(err => {
         res.send(err)
+    })
+})
+
+// Create vehicle
+app.post('/vehicles/:id', (req, res) => {
+    db.owner.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then((owner) => {
+        owner.createVehicle({
+            vin: 1,
+            make: 'Tesla',
+            model: 'Model X',
+            year: 2019
+        }).then((vehicle) => {
+            res.send(`${vehicle.make} ${vehicle.model} has been created`)
+        }).catch(err => {
+            res.send(err);
+        })
+    }).catch(err => {
+        res.send(err);
+    });
+})
+
+// Update Vehicle by id
+app.put('/vehicles/:id', (req, res) => {
+    db.vehicle.update({
+        year: 2020
+    },
+        {
+            where: {
+                id: req.params.id,
+            },
+        }).then((updated) => {
+            res.send(updated)
+        }).catch(err => {
+            res.send(err);
+        });
+})
+
+// Delete Vehicle By id
+app.delete('/vehicles/:id', (req,res) => {
+    db.vehicle.destory({
+        where: {
+            id:req.params.id
+        }
+    }).then((deleted) => {
+        res.send("Deleted vehicle")
+    }).catch(err => {
+        res.send(err);
     })
 })
 
