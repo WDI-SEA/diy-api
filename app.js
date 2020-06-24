1da48ee2295c793376173667cb8f401a363cb7ac
Eliott R Davidson <fussypuppet@Eliotts-MacBook-Pro.local>
2020-06-23 20:43:25 -0700

    MVP complete

--- app.js
+++ app.js
@@ -1,32 +1,76 @@
 const express = require('express');
 const db = require('./models');
 const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded());
 app.use(methodOverride('_method'));
+
+function errorHandler(error) {
+    console.log("🟥🟥🟥🟥 Error! 🟥🟥🟥🟥");
+    console.log(error);
+}
 
 app.get('/', (req, res) => {
     res.send("Hi home page");
 })
 
 app.get('/characters', (req,res) => {
-    res.send("characters index");
+    db.character.findAll().then(characters => {
+        res.send(JSON.stringify(characters));
+    }).catch(errorHandler);
 })
 
-app.post('/characters', (req,res) => {
-    res.send("character created");
+app.post('/characters', (req,res) => { // send data using 'x-www-form-urlencoded' section of postman
+    db.character.create({
+        name: req.body.name,
+        level: req.body.level,
+        class: req.body.class,
+        race: req.body.race,
+        realm: req.body.realm,
+        preferredSpec: req.body.preferredSpec,
+        dateMade: req.body.dateMade
+    }).then(createdCharacter => {
+        console.log(`🧮🧮 Character ${createdCharacter.name} created 🧮🧮`);
+        res.send(JSON.stringify(createdCharacter));
+    }).catch(errorHandler);
 })
 
-app.get('/characters/:id', (req,res) => {
-    res.send(`view of character ${req.params.id}`);
+app.get('/characters/:id', (req,res) => { // needs to be a url, not a ?= query string
+    console.log(`trying to find ${req.params.id} 🪓🪓🪓🪓🪓`);
+    db.character.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(foundCharacter => {
+        console.log(`Character found! 🧲🧲🧲🧲`)
+        res.send(JSON.stringify(foundCharacter));
+    }).catch(errorHandler);
 })
 
 app.delete('/characters/:id', (req, res) => {
-    res.send(`character ${req.params.id} deleted`);
+    db.character.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(deleteResult => {
+        console.log(`Character Deleted 🚘🚘🚘🚘🚘`);
+        console.log(deleteResult);
+        res.send(JSON.stringify(deleteResult));
+    }).catch(errorHandler);
 })
 
 app.put('/characters/:id', (req, res) => {
-    res.send(`character ${req.params.id} updated`);
+    let updateInfo = req.body;
+    db.character.update(updateInfo, {
+        where: {
+            id: req.params.id
+        }
+    }).then(updateResult => {
+        console.log('🍟🍟🍟🍟🍟🍟 character updated');
+        res.send(JSON.stringify(updateResult));
+    }).catch(errorHandler);
 })
 
 app.listen(3000, () => console.log("Listening at 3000 🔈"));
