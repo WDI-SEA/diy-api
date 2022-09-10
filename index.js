8c30b2600c4c8cd263e11a164c7b486596b32b02
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-10 16:06:20 -0700

    add show page and render hitbox details

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 const express = require('express')
 const db = require('./models')
-const ejsLayouts = require('express-ejs-layouts');
+const ejsLayouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
 const PORT = 3000
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 app.use(ejsLayouts)
 app.use(methodOverride('_method'))
 
 app.get('/', (req,res)=>{
-    res.send('home page')
+    db.hitbox.findAll()
+        .then(hitboxes =>{
+            res.render('index.ejs', {hitboxes})
+        }).catch((err) =>{
+            console.log(err)
+            res.render('404.ejs')
+        })
+})
+
+app.get('/new', (req,res)=>{
+    res.render('new.ejs')
 })
 
 app.post('/hitboxes', (req,res)=>{
-    res.send('make a new hitbox')
+    res.render('new.ejs')
 })
 
-app.get('/hitboxes/:id',(req,res)=>{
-    res.send('show one hitbox')
+app.get('/hitboxes/:id', (req,res)=>{
+    db.hitbox.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(hitboxes=>{
+            res.render('show.ejs', {hitboxes})
+        }).catch(err => {
+        console.log(err)
+        res.render('404.ejs')
+    })
 })
-
 app.put('/hitboxes/:id', (req,res)=>{
     res.send('edit a hitbox')
 })
