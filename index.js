1c5cb5c5d23b9f7b9a99a5558b28308602576d70
devin-lynch <devin.l.lynch@gmail.com>
2022-09-10 17:48:17 -0500

    Set index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+const app = express()
+const db = reuire('./models')
+
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+appuse(methodOverride('_method'))
+
+const PORT = 3000
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Tuned into port ${PORT}`)
+})
