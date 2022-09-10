568de78266e290d1b54349afcc7cb58900b18424
joshdewhurst <joshuardewhurst@gmail.com>
2022-09-10 13:23:02 -0700

    db created and model migrated

---
+++ migrations/20220910202006-create-god.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('gods', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      godOf: {
+        type: Sequelize.STRING
+      },
+      img_url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('gods');
+  }
+};
