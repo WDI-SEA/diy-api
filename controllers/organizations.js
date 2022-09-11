273a2ed72a61f605dc5d1e38fb8628384e111342
JasonXChen <jasonxchen888@gmail.com>
2022-09-10 23:46:32 -0400

    Implement post route for organizations

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -15,5 +15,24 @@ router.get("/", async (req, res) =>
         res.send("server error");
     }
 })
+router.post("/", async (req, res) =>
+{
+    try 
+    {
+        res.json(await db.organization.create(    // send json data of org created
+        {
+            // can be replaced with req.body.name, req.body.founded, etc. once form is made
+            name: "Gen.G Esports",
+            founded: "2017-08-11",
+            location: "South Korea",
+            abbreviation: "Gen.G"
+        }));
+    } 
+    catch (error) 
+    {
+        console.warn(error);
+        res.send("server error");
+    }
+})
 
 module.exports = router;
