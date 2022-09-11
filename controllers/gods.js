44616db9fb8336dcbae5c10a6d6bfa7d1d378ef0
joshdewhurst <joshuardewhurst@gmail.com>
2022-09-11 14:26:21 -0700

    trying router.put methods

--- controllers/gods.js
+++ controllers/gods.js
@@ -44,4 +44,49 @@ router.post('/', (req, res) => {
     })
   })
 
+  router.get('/edit/:id', (req,res) => {
+    db.god.findOne({
+        where: { id: req.params.id }
+    })
+    .then((god) => {
+        res.render('gods/edit', {
+            god: god,
+            id: req.params.id
+        })
+    })
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const [god, godCreated] = await db.god.findOrCreate({
+        where: { id: req.params.id }
+      })
+    const updateGod = await scrollBy.god.update({
+        name: req.body.name,
+        godOf: reqbody.godOf,
+        img_url: req.body.img_url
+    })
+    res.redirect(`/gods/${req.params.id}`)
+    } catch (err) {
+        console.log(err)
+        res.send('server error, the gods are at it again')
+    }
+})
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const numRowsDeleted = await db.god.destroy({
+            where: {
+                id: req.params.id
+            }
+        })       
+        res.redirect("/gods")
+
+    } catch (err) {
+        console.log(err)
+        res.send("server error")
+    }
+})
+
 module.exports = router;
