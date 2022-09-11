886d92895a36d9205bf111afb219c4086c554d95
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-10 17:29:49 -0700

    all functions except PUT functional

--- index.js
+++ index.js
@@ -24,8 +24,21 @@ app.get('/new', (req,res)=>{
     res.render('new.ejs')
 })
 
-app.post('/hitboxes', (req,res)=>{
-    res.render('new.ejs')
+app.post('/hitboxes', async (req,res)=>{
+    try{
+        const newHitbox = await db.hitbox.create({
+            name: req.body.name,
+            length: req.body.length,
+            width: req.body.width,
+            height: req.body.height,
+            angle: req.body.angle
+        })
+        console.log(newHitbox)
+        res.redirect('/')
+    }catch(err){
+        console.log(err)
+        res.render('404.ejs')
+    }
 })
 
 app.get('/hitboxes/:id', (req,res)=>{
@@ -41,12 +54,33 @@ app.get('/hitboxes/:id', (req,res)=>{
         res.render('404.ejs')
     })
 })
-app.put('/hitboxes/:id', (req,res)=>{
-    res.send('edit a hitbox')
+
+app.put('/hitboxes/:id', async (req,res)=>{
+    try {
+        await db.hitbox.update({
+            where:{
+                id: req.params.id
+            }
+        })
+        res.redirect('edit.ejs', {hitbox})
+    } catch (error) {
+        console.log(error)
+        res.render('404.ejs')
+    }
 })
 
-app.delete('/hitboxes/:id', (req,res)=>{
-    res.send('delete a hitbox')
+app.delete('/hitboxes/:id', async (req,res)=>{
+    try {
+        await db.hitbox.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.redirect('/')
+    }catch (err) {
+        console.log(err)
+        res.render('404.ejs')
+    }
 })
 
 app.listen(PORT, () =>{
