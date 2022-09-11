a21e68d4efcb327885916ddc519a225231a7879c
Moodybleu <meganbenn27@icloud.com>
2022-09-11 11:02:25 -0700

    Adds views folder and compelete ERD

--- models/pattern.js
+++ models/pattern.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.pattern.belongsToMany(models.dachshund)
     }
   }
   pattern.init({
