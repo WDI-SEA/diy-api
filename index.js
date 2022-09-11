5b79bc9f0c77d4d19c1e179b86e3e7a2d0978c16
Moodybleu <meganbenn27@icloud.com>
2022-09-11 08:36:27 -0700

    Stubbed out routes, created db, and added axios

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ app.set ('view engine', 'ejs')
 // app.use()
 // define express routes
 app.get('/', (req, res) => {
-     res.send('Visitors on / route')
+     res.send('<h1>Build a Dachshund!</h1>')
+})
+app.get('/about', (req, res) => {
+    res.send('<h1>Hello, my name is Hydrangea Benn</h1>')
+})
+app.get('/color', (req, res) =>{
+    res.send('This is where you will see the different color options')
+})
+app.get('/pattern', (req, res) => {
+    res.send('This is where you will see the different pattern options')
+})
+app.get('/size', (req, res) => {
+    res.send('This is where you will see the different size options')
 })
 // tell express to listen on a port for incoming http requests
 app.listen(PORT, () => {
