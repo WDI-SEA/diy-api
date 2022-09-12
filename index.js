fe24248dc5d76e1f9c064bf850a0afd26f03e749
devin-lynch <devin.l.lynch@gmail.com>
2022-09-12 00:59:32 -0500

    set put route but not posting with req.params.name

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/', async (req, res) => {
 app.use('/mixedmartialartists', require('./controllers/mixedmartialartists'))
 
 
-
+// 
 app.listen(PORT, () => {
     console.log(`Tuned into port ${PORT}`)
 })
