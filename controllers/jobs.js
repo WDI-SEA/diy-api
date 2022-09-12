c0e145e1121d3fc346654e7dcb2c17bbb2f3ec1c
aimeepham14 <aimee.pham14@gmail.com>
2022-09-11 23:51:58 -0700

    created new form

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -10,7 +10,6 @@ router.get('/', async (req, res) => {
 //CREATING A NEW JOB LISTING TO TRACK
 router.get('/new', async (req, res) => {
     res.render('jobs/new')
-    console.log('show new jobs here')
 })
 
 module.exports = router
