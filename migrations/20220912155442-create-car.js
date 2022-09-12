a8da9a6e9e66b93709cdcf8702a2a70d4b2167be
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-12 08:58:20 -0700

    add car model

---
+++ migrations/20220912155442-create-car.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cars', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      img_url: {
+        type: Sequelize.STRING
+      },
+      hitboxId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cars');
+  }
+};
--- models/car.js
+++ models/car.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class hitbox extends Model {
+  class car extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,18 +11,16 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.hitbox.hasMany(models.car)
+      models.car.belongsTo(models.hitbox)
     }
   }
-  hitbox.init({
+  car.init({
     name: DataTypes.STRING,
-    length: DataTypes.STRING,
-    width: DataTypes.STRING,
-    height: DataTypes.STRING,
-    angle: DataTypes.STRING
+    img_url: DataTypes.STRING,
+    hitboxId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'hitbox',
+    modelName: 'car',
   });
-  return hitbox;
+  return car;
 };
